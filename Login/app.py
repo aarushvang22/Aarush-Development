71bdad348c0f2d369c5a6e27023c379320d19334
Kvanf <101415252+Kvanf@users.noreply.github.com>
2025-04-06 15:15:05 -0400

    Added multiple usernames & passwords

--- Login/app.py
+++ Login/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, redirect, url_for, render_template, session
 
-app = Flask("Login Simulation")
+app = Flask(__name__)
 
+loginInformation = {"admin": "admin123", "aarush": "aarushlovescybersecurity", "aarav": "aaravlovesIT", "harsh": "harshlovescoding"}
 @app.route("/")
 def home():
     return redirect(url_for("login"))
@@ -11,7 +12,7 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        if username == "admin" and password == "admin123":
+        if username in loginInformation and password == loginInformation[username]:
             #session["user"] = username
             return redirect(url_for("dashboard"))
         else:
